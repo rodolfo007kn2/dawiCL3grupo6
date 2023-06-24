ddfcc26808765dfdb1b332811bac4dcdf12c8b72
Airthon1234 <cipoteland@gmail.com>
2023-06-24 13:54:37 -0500

    usuario_commit

--- src/main/java/com/cibertec/dawiCL3grupo6/model/bd/Usuario.java
+++ src/main/java/com/cibertec/dawiCL3grupo6/model/bd/Usuario.java
@@ -1,5 +1,39 @@
 package com.cibertec.dawiCL3grupo6.model.bd;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name="usuario")
 public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer codigo;
+	@Column(name="nombre")
+	private String nombre;
+	@Column(name="apellido")
+	private String apellido;
+	@Column(name="email")
+	private String email;
+	@Column(name="usuario")
+	private String usuario;
+	@Column(name="contrasena")
+	private String contrasena;
 
 }
