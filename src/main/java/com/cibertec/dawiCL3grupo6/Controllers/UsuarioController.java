9f0263b6eea1596347e4863a0bd248a2132fb298
rodolfo007kn2 <123rod123@gmail.com>
2023-06-24 14:34:56 -0500

    controladores

--- src/main/java/com/cibertec/dawiCL3grupo6/Controllers/UsuarioController.java
+++ src/main/java/com/cibertec/dawiCL3grupo6/Controllers/UsuarioController.java
@@ -1,9 +1,34 @@
 package com.cibertec.dawiCL3grupo6.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.cibertec.dawiCL3grupo6.model.bd.Usuario;
+
+@RequestMapping("/auth")
+@Controller
 public class UsuarioController {
 
-
+	@GetMapping("/login")
+public String frmlogin() {
 	
+	return "auth/frmLogin";
+}
 	
+	@GetMapping("/registrarusuario")
+	public String frmguardar() {
+		
+		return"auth/frmRegistro";
+	}
+	
+	@PostMapping("/guardarUsuario")
+	public String guardarusuario(@ModelAttribute Usuario usuario, Model model) {
+		
+		
+		return"auth/frmRegistro";
+	}
 }
