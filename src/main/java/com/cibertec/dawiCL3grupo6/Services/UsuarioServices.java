a46f155808b5c45fff0e53f1eb84dc0085f9c86e
angelmi11 <angelgnr11@gmail.com>
2023-06-24 18:06:38 -0500

    se agrego el listado de usuarios en home

--- src/main/java/com/cibertec/dawiCL3grupo6/Services/UsuarioServices.java
+++ src/main/java/com/cibertec/dawiCL3grupo6/Services/UsuarioServices.java
@@ -2,6 +2,8 @@ package com.cibertec.dawiCL3grupo6.Services;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -10,16 +12,15 @@ import com.cibertec.dawiCL3grupo6.Repositorys.UsuarioRepository;
 import com.cibertec.dawiCL3grupo6.model.bd.Rol;
 import com.cibertec.dawiCL3grupo6.model.bd.Usuario;
 
-
 @Service
 public class UsuarioServices {
 
-	@Autowired 
+	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
+
 	@Autowired
 	private RolRepository rolRepository;
-	
+
 	private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 
 	public Usuario buscarUsuarioPorNomusuario(String nomusuario) {
@@ -28,10 +29,14 @@ public class UsuarioServices {
 
 	public Usuario guardarUsuario(Usuario usuario) {
 		usuario.setPassword(bCryptPasswordEncoder.encode(usuario.getPassword()));
-		
+
 		usuario.setActivo(true);
 		Rol rol = rolRepository.findByNomrol("digitador");
 		usuario.setRoles(new HashSet<Rol>(Arrays.asList(rol)));
 		return usuarioRepository.save(usuario);
+	}
+
+	public List<Usuario> listarUsuarios() {
+		return usuarioRepository.findAll();
 	}
 }
